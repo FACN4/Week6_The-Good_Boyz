228f99a4f8d0721c5c06dbf1f74612a8e43cfd05
Ben Newman <benjamin-newman@live.co.uk>
2018-08-07 14:23:09 +0300

    basic file structure

---
+++ public/xhr.js
@@ -0,0 +1,11 @@
+function xhrAPI(url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function request() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const res = JSON.stringify(xhr.responseText);
+      callback(res);
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+}
---
+++ src/handler.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const fs = require("fs");
+const httpReq = require("./httpReq.js");
+
+const handler500 = res => {
+  res.writeHead(500, { "content-type": "text/plain" });
+  res.end("server error");
+};
+
+const handlerHome = (req, res) => {
+  fs.readFile(
+    path.join(__dirname, "..", "public", "index.html"),
+    (err, file) => {
+      if (err) {
+        handler500(res);
+      } else {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end(file);
+      }
+    }
+  );
+};
+
+const handlerPublic = (req, res, url) => {
+  const extension = url.split(".")[1];
+  const extensionType = {
+    html: "text/html",
+    css: "text/css",
+    js: "application/javascript",
+    jpg: "image/jpg",
+    gif: "image/gif"
+  };
+    pathfile = path.join(__dirname, "..", "public", url);
+  }
+  fs.readFile(pathfile, (err, file) => {
+    if (err) {
+      handler500(res);
+    } else {
+      res.writeHead(200, { "Content-Type": extensionType[extension] });
+      res.end(file);
+    }
+  });
+};
+// const handlerExchange = (req, res) => {
+//   const query = req.url.split("=")[1];
+//   httpReq(query, (error, body) => {
+//     if (error) {
+//       console.log(error);
+//       return;
+//     }
+//     res.end(body);
+//   });
+// };
+
+const handler404 = (req, res) => {
+  fs.readFile(path.join(__dirname, "..", "public", "404.html"), (err, file) => {
+    if (err) {
+      handler500(res);
+    } else {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.end(file);
+    }
+  });
+};
+
+module.exports = {
+  handlerHome,
+  handlerPublic,
+  // handlerExchange,
+  handler404
+};
