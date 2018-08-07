51fb3488904a552cfaa506139812a3db264a29ec
Ray <ryan.natoor@gmail.com>
2018-08-07 16:24:51 +0300

    #4 added build js and build sql files

---
+++ src/database/build.sql
@@ -0,0 +1,22 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users, movies, reviews, likes;
+
+
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(10) NOT NULL,
+  last_name VARCHAR(10) NOT NULL,
+);
+
+CREATE TABLE reviews(
+  id SERIAL PRIMARY KEY,
+  reviews TEXT NOT NULL,
+);
+
+CREATE TABLE movies (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100)
+);
+
+COMMIT;
