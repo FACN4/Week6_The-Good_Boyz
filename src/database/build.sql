8957e60fd48ffb85b36f881e79bd10663d0b4146
Ray <ryan.natoor@gmail.com>
2018-08-08 12:23:17 +0300

    build file

--- src/database/build.sql
+++ src/database/build.sql
@@ -1,17 +1,18 @@
 BEGIN;
 
-DROP TABLE IF EXISTS users, movies, reviews, likes;
+DROP TABLE IF EXISTS movies, reviews;
 
 CREATE TABLE reviews(
   id SERIAL PRIMARY KEY,
   movie_id INTEGER NOT NULL REFERENCES movies(id),
   full_name VARCHAR(20),
-  reviews TEXT NOT NULL
+  review TEXT NOT NULL
+  rating INTEGER CHECK (rating > 0 AND rating < 6)
 );
 
 CREATE TABLE movies (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(100)
+  name VARCHAR(100) UNIQUE
 );
 
 
